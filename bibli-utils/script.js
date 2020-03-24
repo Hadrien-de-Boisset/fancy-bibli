11b080cbbc9b39b7599abf56f317169c3632c400
Hadrien-de-Boisset <62590595+Hadrien-de-Boisset@users.noreply.github.com>
2020-03-24 09:07:27 +0100

    Upload v0.1
    Initial upload

---
+++ bibli-utils/script.js
@@ -0,0 +1,31 @@
+$(function() {
+
+    $("li>ol").hide();
+
+    $(".unroll").on("click", function(event) {
+
+        event.stopPropagation();
+
+        $(".visible").off("click", blockProgression)
+
+        let depth = $(this).data("depth");
+
+        let trigeredSlide = $(this).children("ol:first-of-type")
+
+        $("li[data-depth='" + (depth) + "']>ol").not(trigeredSlide).slideUp();
+        $("li[data-depth='" + (depth) + "']>ol").not(trigeredSlide).removeClass("visible")
+
+        console.log("li[data-depth='" + (depth) + "']>ol");
+
+        trigeredSlide.slideToggle();
+        trigeredSlide.addClass("visible");
+        trigeredSlide.on("click", blockProgression)
+    })
+
+    function blockProgression(event) {
+
+        event.stopPropagation();
+
+    };
+
+});
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+    function tableauContenu($dos, $depth){
+   
+    
+        $fichiers = scandir($dos); // crée un tableau php qui contient la liste des fichiers
+        
+        $fichiers = array_diff($fichiers, array('..', '.','index.php', 'bibli-utils')); // si le serveur utilise linux, enlève les pseudo-fichiers de navigation "." et ".."
+        
+        if(empty($fichiers)){
+            
+            return false;
+            
+        }
+        
+        $fichiers = array_values ( $fichiers ); // renumérotte tout au cas ou la ligne d'avant à enlevé des lignes innutiles
+
+        $resultat = "\n<ol>\n"; //ouvre la liste
+        
+        foreach($fichiers as $nom){
+            
+            if(is_dir($dos."/".$nom) && !empty(array_diff(scandir($dos."/".$nom), array('..', '.')))){
+                
+                $resultat .= "<li data-depth='$depth' class='unroll'><span class='dossier'>".$nom."</span>".tableauContenu($dos."/".$nom, $depth+1)."</li>\n";
+                
+            }
+            else if(is_dir($dos."/".$nom)){
+                
+                $resultat .= "<li data-depth='$depth'><span class='dossier dossier-vide'>".$nom."</span></li>\n";
+                
+            }
+            else{
+                
+                $resultat .= "<li data-depth='$depth'><a href='".$dos."/".$nom."' download>".$nom."</a></li>\n";
+                
+            }
+            
+            
+        }
+        
+        $resultat .= "</ol>\n"; //ferme la liste
+        
+        return $resultat;
+        
+    
+    }
+    
+    
+    $dossier = getcwd(); // chemin du dossier
+    
+    $output = tableauContenu($dossier, 0);
+    
+    include "bibli-utils/index.phtml";
+    
+   
+?>
